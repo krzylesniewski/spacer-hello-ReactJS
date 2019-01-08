99d4abfa2b944eb1a27cf8bce1f2b5e7d9bb653c
Krzysztof Leśniewski <krzysztof.lesniewski.wroc@gmail.com>
2019-01-08 21:55:49 +0100

    Basic view. Route added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
+import {
+  BrowserRouter as Router, Route, Link
+} from 'react-router-dom';
+import Home from './views/Home';
+import About from './views/About';
+
+
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-      <h1>Greenfield</h1>
-      </div>
+      <Router>
+        <div className="container">
+          <ul>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/about">About</Link></li>
+          </ul>
+          <Route exact path="/" component={Home}/>
+          <Route path="/about" component={About}/>
+
+        </div>
+      </Router>
     );
   }
 }
