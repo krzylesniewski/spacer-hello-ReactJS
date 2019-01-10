e103054c21ea5a5a1e2d145917fecf2ddd178b6c
Krzysztof Leśniewski <krzysztof.lesniewski.wroc@gmail.com>
2019-01-10 21:37:52 +0100

    basic view + draft of style

--- src/views/Home.js
+++ src/views/Home.js
@@ -2,13 +2,52 @@ import React, {Component} from 'react';
 
 
 class Home extends Component {
+  state = {
+    value: '',
+  };
+  inputChangeHandler = (e) => {
+    this.setState({
+      value: e.target.value
+    });
+  };
+
   render() {
     return (
-      <div className="container">
-        <h1>Home</h1>
+      <div className="wrapper" style={Wrapper}>
+        <div style={Search}>
+          <label htmlFor="search">Search</label>
+          <input type="text"
+                 name="search"
+                 id="search"
+                 onChange={this.inputChangeHandler}
+                 style={Input}/>
+        </div>
       </div>
     )
   }
+
+
+
+
 }
+const Wrapper = {
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  margin: '0',
+  padding: '30px',
+  width: '100%',
+};
+const Search = {
+  display: 'flex',
+  flexDirection: 'column',
+  width: '250px',
+};
+const Input = {
+  height: '30px',
+  border: '0',
+  borderBottom: '1px solid black',
+};
+
 
 export default Home;
